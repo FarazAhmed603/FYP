24cc001ce7358232e229a9b627ead535ad1eaf83
usama idrees <usamachaudhary12@gmail.com>
2023-02-01 13:08:47 +0500

    git set

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 const ENV = {
-  // IP: 'http://a7f5-110-39-25-94.ngrok.io/',
-  IP: 'http://192.168.100.21:4000/',
+  IP: 'http://a7f5-110-39-25-94.ngrok.io/',
+  // IP: 'http://192.168.100.21:4000/',
 };
 export default ENV;
